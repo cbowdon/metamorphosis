f7b2ff5e77c08e3790b78f109145fcd739841cc6
Chris Bowdon <cbowdon@prefect.local>
2018-04-28 13:08:36 +0100

    Initialize line-split as a clone of pipe

--- src/metamorphosis/line_split.clj
+++ src/metamorphosis/line_split.clj
@@ -3,4 +3,17 @@
            [org.apache.kafka.streams KafkaStreams StreamsBuilder StreamsConfig]
            [org.apache.kafka.common.serialization Serdes]))
 
-(def streams nil)
+(def props
+  (doto (new Properties)
+    (.put StreamsConfig/APPLICATION_ID_CONFIG "streams-linesplit")
+    (.put StreamsConfig/BOOTSTRAP_SERVERS_CONFIG "localhost:9092")
+    (.put StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG (class (Serdes/String)))
+    (.put StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG (class (Serdes/String)))))
+
+(def builder (new StreamsBuilder))
+(-> builder
+    (.stream "streams-plaintext-input")
+    (.to "streams-linesplit-output"))
+(def topology (.build builder))
+
+(def streams (new KafkaStreams topology props))
